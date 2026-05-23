230e15921a5ecc7fb84ec19500d1efd5c81e53fc
Sho Lee <the235style@yahoo.com>
2026-05-23 11:47:35 +0900

    Updated

--- zh/tools/admin/assets/js/data.js
+++ zh/tools/admin/assets/js/data.js
@@ -73,6 +73,172 @@ const services = [
         }
       ]
     }
+  },
+
+  // -------------------------------------------------------
+  // www.koreatalk.tw
+  // -------------------------------------------------------
+  {
+    id: "koreatalk-tw",
+    name: "韓國通 (KoreaTalk)",
+    category: "KoreaTalk",
+    links: {
+      main: [
+        {
+          url_external: "https://www.koreatalk.tw/",
+          url_localhost: "./../../../../koreatalk-tw/"
+        }
+      ],
+      usage: [
+        {
+          url_external: "https://www.koreatalk.tw/usage/",
+          url_localhost: "./../../../../koreatalk-tw/usage/"
+        }
+      ],
+      store: [
+        {
+          url_external: "https://www.koreatalk.tw/store/",
+          url_localhost: "./../../../../koreatalk-tw/store/"
+        }
+      ],
+      marketing: [
+        {
+          url_external: "https://www.koreatalk.tw/marketing/",
+          url_localhost: "./../../../../koreatalk-tw/marketing/"
+        }
+      ],
+      school: [
+        {
+          url_external: "https://www.koreatalk.tw/school/",
+          url_localhost: "./../../../../koreatalk-tw/school/"
+        }
+      ],
+      data: [
+        {
+          url_external: "https://www.koreatalk.tw/data/a/",
+          url_localhost: "./../../../../koreatalk-tw/data/a/"
+        }
+      ],
+      payinfo: [
+        {
+          url_external: "https://www.koreatalk.tw/payinfo/",
+          url_localhost: "./../../../../koreatalk-tw/payinfo/"
+        }
+      ],
+      line: [
+        {
+          url_external: "https://www.koreatalk.tw/line/",
+          url_localhost: "./../../../../koreatalk-tw/line/"
+        }
+      ],
+      youtube: [
+        {
+          url_external: "https://www.koreatalk.tw/yt/",
+          url_localhost: "./../../../../koreatalk-tw/yt/"
+        }
+      ],
+      instagram: [
+        {
+          url_external: "https://www.koreatalk.tw/ig/",
+          url_localhost: "./../../../../koreatalk-tw/ig/"
+        }
+      ],
+      facebook: [
+        {
+          url_external: "https://www.koreatalk.tw/fb/",
+          url_localhost: "./../../../../koreatalk-tw/fb/"
+        }
+      ],
+      threads: [
+        {
+          url_external: "https://www.koreatalk.tw/threads/",
+          url_localhost: "./../../../../koreatalk-tw/threads/"
+        }
+      ]
+    }
+  },
+
+  // -------------------------------------------------------
+  // www.japantalk.tw
+  // -------------------------------------------------------
+  {
+    id: "japantalk-tw",
+    name: "日本通 (JapanTalk)",
+    category: "JapanTalk",
+    links: {
+      main: [
+        {
+          url_external: "https://www.japantalk.tw/",
+          url_localhost: "./../../../../japantalk-tw/"
+        }
+      ],
+      usage: [
+        {
+          url_external: "https://www.japantalk.tw/usage/",
+          url_localhost: "./../../../../japantalk-tw/usage/"
+        }
+      ],
+      store: [
+        {
+          url_external: "https://www.japantalk.tw/store/",
+          url_localhost: "./../../../../japantalk-tw/store/"
+        }
+      ],
+      marketing: [
+        {
+          url_external: "https://www.japantalk.tw/marketing/",
+          url_localhost: "./../../../../japantalk-tw/marketing/"
+        }
+      ],
+      school: [
+        {
+          url_external: "https://www.japantalk.tw/school/",
+          url_localhost: "./../../../../japantalk-tw/school/"
+        }
+      ],
+      data: [
+        {
+          url_external: "https://www.japantalk.tw/data/a/",
+          url_localhost: "./../../../../japantalk-tw/data/a/"
+        }
+      ],
+      payinfo: [
+        {
+          url_external: "https://www.japantalk.tw/payinfo/",
+          url_localhost: "./../../../../japantalk-tw/payinfo/"
+        }
+      ],
+      line: [
+        {
+          url_external: "https://www.japantalk.tw/line/",
+          url_localhost: "./../../../../japantalk-tw/line/"
+        }
+      ],
+      youtube: [
+        {
+          url_external: "https://www.japantalk.tw/yt/",
+          url_localhost: "./../../../../japantalk-tw/yt/"
+        }
+      ],
+      instagram: [
+        {
+          url_external: "https://www.japantalk.tw/ig/",
+          url_localhost: "./../../../../japantalk-tw/ig/"
+        }
+      ],
+      facebook: [
+        {
+          url_external: "https://www.japantalk.tw/fb.kr/",
+          url_localhost: "./../../../../japantalk-tw/fb.kr/"
+        }
+      ],
+      threads: [
+        {
+          url_external: "https://www.japantalk.tw/threads/",
+          url_localhost: "./../../../../japantalk-tw/threads/"
+        }
+      ]
+    }
   }
 ];
 
